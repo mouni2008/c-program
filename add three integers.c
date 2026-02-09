f62c82d81df5f92478ffaa3bd22f2321f410a4d9
mouni2008 <prakashsridharm@gmail.com>
2026-02-09 16:39:55 +0530

    Add files via upload

---
+++ add three integers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a=2 , b=3,c=5;
+    int d=1 , e=4,f=5;
+    int g=3 ,h=3,i=4;
+    printf("%d",a+b+c);
+    printf("\n%d",d+e+f);
+    printf("\n%d",g+h+i);
+   
+    return 0;
+}
