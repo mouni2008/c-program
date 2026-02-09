680e0f9e9eedc7416ca311c4d619374ba3ee0cab
mouni2008 <prakashsridharm@gmail.com>
2026-02-09 16:13:59 +0530

    Add files via upload

---
+++ two integers mul .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a=4, b=5;
+    int c=10, d=2;
+    int e=1,f=20;
+    printf("%d",a*b);
+    printf("\n%d",c*d);
+    printf("\n%d",e*f);
+    return 0;
+}
