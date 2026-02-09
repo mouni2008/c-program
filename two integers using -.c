e67ce9ef855635f42ffbe348b4a0ea6a077d6006
mouni2008 <prakashsridharm@gmail.com>
2026-02-09 16:11:21 +0530

    Add files via upload

---
+++ two integers using -.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a=8, b=3;
+    int c=10, d=5;
+    int e=15,f=10;
+    printf("%d",a-b);
+    printf("\n%d",c-d);
+    printf("\n%d",e-f);
+    return 0;
+}
