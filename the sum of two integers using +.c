d98fdd257084cb5fc1206112fd082a9c41f80374
mouni2008 <prakashsridharm@gmail.com>
2026-02-09 16:09:08 +0530

    Add files via upload

---
+++ the sum of two integers using +.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a=5, b=5;
+    int c=3, d=7;
+    int e=8,f=2;
+    printf("%d",a+b);
+    printf("\n%d",c+d);
+    printf("\n%d",e+f);
+    return 0;
+}
