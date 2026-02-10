99aabcca7ddb7ddced485d3540bad5d530909b5f
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 12:14:43 +0530

    Add files via upload

---
+++ two conditions unisg AND and OR.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a=5,b=10;
+    int c=0,d=5;
+    int e=0,f=0;
+   
+    printf("%d",a&&b && a||b);
+ 
+    printf("\n%d",c&&d && c||d);
+   
+    printf("\n%d",e&&f && e||f);
+  
+    
+    
+    
+    return 0;
+    
+}
