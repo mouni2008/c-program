95930f7691118be373c445028603c3928e9c4e25
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 12:01:27 +0530

    Add files via upload

---
+++ logical or.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=0,b=5;
+    int c=0,d=0;
+    int e=3,f=7;
+    printf("%d",a||b);
+    printf("\n%d",c||d);
+    printf("\n%d",e||f);
+    
+    
+    
+    return 0;
+    
+}
