2df6c40286800d5127e6fc101ce704ba0284d727
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 12:05:15 +0530

    Add files via upload

---
+++ logical not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a=0;
+    int b=0;
+    int c=0;
+   
+    printf("%d",!a);
+    printf("\n%d",!b);
+    printf("\n%d",!c);
+    
+    
+    
+    return 0;
+    
+}
