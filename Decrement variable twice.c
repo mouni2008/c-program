87527aec818869068122cb60c8ad3265a4921274
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 11:27:33 +0530

    Add files via upload

---
+++ Decrement variable twice.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int a=6;
+    --a;
+    --a;
+    printf("%d",a);
+    
+    int b=2;
+    --b;
+    --b;
+    printf("\n%d",b);
+    
+    int c=10;
+    --c;
+    --c;
+    printf("\n%d",c);
+    
+    
+    
+    return 0;
+    
+}
