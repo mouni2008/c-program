bcc96f2ac7c86c6ee2d649c15de4344bd420ddb1
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 11:45:13 +0530

    Add files via upload

---
+++ compare equal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=5,b=5;
+    int c=3,d=4;
+    int e=0,f=0;
+    printf("%d",a==b);
+    printf("\n%d",c==d);
+    printf("\n%d",e==f);
+    
+    
+    
+    return 0;
+    
+}
