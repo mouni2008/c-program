59033b720b0412d3a63f5f06ddbe50464ce22004
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 12:06:53 +0530

    Add files via upload

---
+++ non zero values for not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    int b=1;
+    int c=-3;
+   
+    printf("%d",!a);
+    printf("\n%d",!b);
+    printf("\n%d",!c);
+    
+    
+    
+    return 0;
+    
+}
