40c351ef01bc312edd7cc10f515a636c6fa6ee1a
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 11:42:59 +0530

    Add files via upload

---
+++ compare lessthan.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=5,b=10;
+    int c=9,d=2;
+    int e=4,f=4;
+    printf("%d",a<b);
+    printf("\n%d",c<d);
+    printf("\n%d",e<f);
+    
+    
+    
+    return 0;
+    
+}
