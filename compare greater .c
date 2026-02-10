f845a8e8ffb07bbc43dc47db55a73806ef8c5aa5
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 11:34:56 +0530

    Add files via upload

---
+++ compare greater .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=5;
+    int c=3,d=8;
+    int e=7,f=7;
+    printf("%d",a>b);
+    printf("\n%d",c>d);
+    printf("\n%d",e>f);
+    
+    
+    
+    return 0;
+    
+}
