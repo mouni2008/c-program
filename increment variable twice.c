21826b714c00d8e9f97c56329b38284d7d0036c0
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 11:25:19 +0530

    Add files via upload

---
+++ increment variable twice.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    a++;
+    a++;
+    printf("%d",a);
+    
+    int b=0;
+    b++;
+    b++;
+    printf("\n%d",b);
+    
+    int c=10;
+    c++;
+    c++;
+    printf("\n%d",c);
+    
+    
+    
+    return 0;
+    
+}
