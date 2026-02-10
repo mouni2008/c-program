9d8efebc581a4e0698077f04b713f902edd481af
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 11:51:22 +0530

    Add files via upload

---
+++ compare less and greater.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a=5,b=5;
+    int c=10,d=5;
+    int e=3,f=7;
+    printf("%d ",a>=b);
+    printf("%d",a<=b);
+    printf("\n%d ",c>=d);
+    printf("%d",c<=d);
+    printf("\n%d ",e>=f);
+    printf("%d",e<=f);
+    
+    
+    
+    return 0;
+    
+}
