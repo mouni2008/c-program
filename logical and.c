ba71c900d4b6e3a1c95751d2f47a970c8d43b90d
mouni2008 <prakashsridharm@gmail.com>
2026-02-10 11:59:42 +0530

    Add files via upload

---
+++ logical and.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=5,b=3;
+    int c=1,d=1;
+    int e=0,f=5;
+    printf("%d",a&&b);
+    printf("\n%d",c&&d);
+    printf("\n%d",e&&f);
+    
+    
+    
+    return 0;
+    
+}
