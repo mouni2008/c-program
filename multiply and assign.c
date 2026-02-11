16d7854d58ef8574faa7815ce67250ff50c8f1d4
mouni2008 <prakashsridharm@gmail.com>
2026-02-11 09:54:50 +0530

    Add files via upload

---
+++ multiply and assign.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ int main(){
+     
+     int a=4, b=5;
+     int c=3, d=3;
+     int e=10,f=2;
+     
+     printf("%d",a*=b);
+     printf("\n%d",c*=d);
+     printf("\n%d",e*=f);
+     return 0; 
+ }
+ 
+ 
