3e8a2751c5387bb0d1b08d33435b31225d51b7d4
mouni2008 <prakashsridharm@gmail.com>
2026-02-11 16:09:02 +0530

    Add files via upload

---
+++ check divisible by five.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num;
+   
+    scanf("%d",&num);
+    
+    if(num % 5== 0){
+        printf("number is divisible by 5");
+    }
+    else{
+        printf("number is not divisible by 5");
+    }
+    return 0;
+}
