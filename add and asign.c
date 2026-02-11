e2a24fb941c8c9ae3d0b17f96efc443b3d86734c
mouni2008 <prakashsridharm@gmail.com>
2026-02-11 09:31:37 +0530

    Add files via upload

---
+++ add and asign.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ int main(){
+     
+     int a=10, b=5;
+     int c=3, d=7;
+     int e=0,f=4;
+     
+     printf("%d",a+=b);
+     printf("\n%d",c+=d);
+     printf("\n%d",e+=f);
+     return 0;
+ }
