ebb600416e6defbe5b7a710c241e287c86b2a9e8
mouni2008 <prakashsridharm@gmail.com>
2026-02-11 09:50:50 +0530

    Add files via upload

---
+++ subtract and assign.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main(){
+     
+     int a=10, b=5;
+     int c=20, d=10;
+     int e=5,f=5;
+     
+     printf("%d",a-=b);
+     printf("\n%d",c-=d);
+     printf("\n%d",e-=f);
+     return 0; 
+ }
+ 
+ 
