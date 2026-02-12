fc0ccc06777a52bb10bb42c8831c16ca6375105d
mouni2008 <prakashsridharm@gmail.com>
2026-02-12 15:19:00 +0530

    Add files via upload

---
+++ simple calculator.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+
+    char i;
+    int a,b;
+    scanf("%d%c%d",&a,&i,&b);
+    switch (i){
+        case '+':
+        printf("Result = %d",a + b);
+        break;
+
+        case '-':
+        printf("Result =%d",a - b);
+        break;
+
+        case '*':
+        printf("Result =%d",a * b);
+        break;
+
+        case'/':
+        printf("Result =%d",a / b);
+        break;
+
+    default:
+        printf("enter invalid operator");
+
+        return 0;
+
+}
+
+    }
+
