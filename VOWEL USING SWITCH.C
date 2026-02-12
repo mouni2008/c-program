ee6bdd71a7647655c51e4e00a74383cef6d58e0e
mouni2008 <prakashsridharm@gmail.com>
2026-02-12 15:42:36 +0530

    Add files via upload

---
+++ VOWEL USING SWITCH.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+
+    
+    char ch;
+    scanf("%c",&ch);
+    switch (ch){
+        case 'A':
+        printf("Vowel");
+        break;
+
+        case 'E':
+        printf("Vowel");
+        break;
+
+        case 'I':
+        printf("Vowel");
+        break;
+
+        case 'O':
+        printf("Vowel");
+        break;
+
+        case 'U':
+        printf("Vowel");
+        break;
+
+        default:
+        printf("Consonant");
+
+    
+
+        return 0;
+
+}
+
+    }
+
