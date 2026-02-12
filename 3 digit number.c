afff0f4facb1f3ce90fe0561766f165c08189f92
mouni2008 <prakashsridharm@gmail.com>
2026-02-12 11:24:56 +0530

    Add files via upload

---
+++ 3 digit number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    
+    int num;
+    scanf("%d",&num);
+    
+    if(num >= 100 && num <=999){
+        printf("three digit number");
+    }
+    else{
+        printf(" invalid num");
+    }
+    return 0;
+}
