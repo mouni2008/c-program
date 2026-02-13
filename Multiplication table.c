cf816b8f1fda2962fa360ba0b691e77a8ef81726
mouni2008 <prakashsridharm@gmail.com>
2026-02-13 11:57:29 +0530

    Add files via upload

---
+++ Multiplication table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+    for(int i=5;i<=50;i+=5){
+
+        printf("%d ",i);
+
+    } 
+
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=10;i++){
+        printf("%d ",i);
+
+    }
+    return 0;
+}
+
+
+
+#include<stdio.h>
+int main(){
+    for(int i=3;i<=30;i+=3){
+
+        printf("%d ",i);
+    }
+    return 0;
+
+}
+
