0d3ca6266aae246cbcd2e9f9b603fb311b928df6
mouni2008 <prakashsridharm@gmail.com>
2026-02-14 14:09:41 +0530

    Add files via upload

---
+++ POWER of a num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int base,power;
+    int result=1;
+    
+    scanf("%d %d",&base,&power);
+
+    for(int i=1;i<=power;i++){
+
+        result=result*base;
+
+    }
+
+    printf("%d",result);
+    
+    
+    return 0;
+}
