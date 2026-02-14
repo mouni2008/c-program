eaefb66bca6f0a0b4881fbfea242e2f2eeee5adb
mouni2008 <prakashsridharm@gmail.com>
2026-02-14 12:11:07 +0530

    Add files via upload

---
+++ SUM of digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    int sum;
+    
+    scanf("%d",&n);
+
+    for(int i=1;i<=3;i++){
+        sum=sum + i;
+
+    }
+
+    
+
+    printf("%d",sum);
+    
+    
+    return 0;
+}
