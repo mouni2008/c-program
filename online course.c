7c005d20611b595fc01ab011312428a2cfcd2e90
mouni2008 <prakashsridharm@gmail.com>
2026-02-26 15:29:36 +0530

    Add files via upload

---
+++ online course.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    int hours;
+    int i = 0;
+    int inactiveWeeks = 0;
+
+    scanf("%d", &N);
+
+    while (i < N) {
+        scanf("%d", &hours);
+
+        if (hours == 0) {
+            inactiveWeeks++;   // Count inactive week
+        }
+
+        i++;
+    }
+
+    printf("Inactive Weeks: %d\n", inactiveWeeks);
+
+    if (inactiveWeeks >= 3) {
+        printf("Risk Status: High\n");
+    } else {
+        printf("Risk Status: Low\n");
+    }
+
+    return 0;
+}
