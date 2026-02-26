1b71481cbd3b4ac71fa3313d9f70d60f90e6f7a7
mouni2008 <prakashsridharm@gmail.com>
2026-02-26 15:09:18 +0530

    Add files via upload

---
+++ mobile data.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int totalData, N;
+    int usage;
+    int i = 0;
+    int daysUsed = 0;
+
+    scanf("%d", &totalData);
+    scanf("%d", &N);
+
+    while (i < N && totalData > 0) {
+        scanf("%d", &usage);
+        totalData -= usage;
+
+        daysUsed++;
+
+        if (totalData <= 0) {
+            totalData = 0;   // Negative values not allowed
+            break;
+        }
+
+        i++;
+    }
+
+    printf("Days Used: %d\n", daysUsed);
+    printf("Remaining Data: %dGB\n", totalData);
+
+    return 0;
+}
